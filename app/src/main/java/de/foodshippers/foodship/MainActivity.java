09e3b2e78a36f42541df02f4dd0cdedeb56dee02
eknoes <me@eknoes.de>
2016-11-19 20:27:14 +0100

    Removed unnecessary menu items

--- app/src/main/java/de/foodshippers/foodship/MainActivity.java
+++ app/src/main/java/de/foodshippers/foodship/MainActivity.java
@@ -149,16 +149,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
 
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
