f8bc69aa46233de6a1f69fbf83b4802a58dad9a5
eknoes <me@eknoes.de>
2016-11-19 20:11:44 +0100

    Added Code Source

--- app/src/main/java/de/foodshippers/foodship/Utils.java
+++ app/src/main/java/de/foodshippers/foodship/Utils.java
@@ -6,6 +6,10 @@ import android.content.SharedPreferences;
  * Created by soenke on 19.11.16.
  */
 public class Utils {
+
+    /**
+     * Source: https://stackoverflow.com/questions/16319237/cant-put-double-sharedpreferences#18098090
+     */
     public static SharedPreferences.Editor putDouble(final SharedPreferences.Editor edit, final String key, final double value) {
         return edit.putLong(key, Double.doubleToRawLongBits(value));
     }
