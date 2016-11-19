95d4909af73ab8c977325190fb4f84239197f0a6
eknoes <me@eknoes.de>
2016-11-19 20:09:27 +0100

    Added Utils to handle doubles in SharedPreferences

---
+++ app/src/main/java/de/foodshippers/foodship/Utils.java
@@ -0,0 +1,17 @@
+package de.foodshippers.foodship;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by soenke on 19.11.16.
+ */
+public class Utils {
+    public static SharedPreferences.Editor putDouble(final SharedPreferences.Editor edit, final String key, final double value) {
+        return edit.putLong(key, Double.doubleToRawLongBits(value));
+    }
+
+    public static double getDouble(final SharedPreferences prefs, final String key, final double defaultValue) {
+        return Double.longBitsToDouble(prefs.getLong(key, Double.doubleToLongBits(defaultValue)));
+    }
+
+}
