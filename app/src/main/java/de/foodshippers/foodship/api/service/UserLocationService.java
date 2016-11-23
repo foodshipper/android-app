753f5195dabf37198aa3e280925f597b25209e1d
eknoes <me@eknoes.de>
2016-11-23 12:43:48 +0100

    Moved UserAPI

--- app/src/main/java/de/foodshippers/foodship/api/service/UserLocationService.java
+++ app/src/main/java/de/foodshippers/foodship/api/service/UserLocationService.java
@@ -8,7 +8,7 @@ import retrofit2.http.Query;
  * Created by soenke on 21.11.16.
  */
 public interface UserLocationService {
-    @PUT("/v1/home-location")
+    @PUT("/v1/user/home-location")
     Call<Void> setHomeLocation(@Query("user_id") String userId, @Query("lat") double latitude, @Query("lon") double longitude);
 
 }
